9335c59c36b94c42bfc75e9fd3986768d1c802c0
rayshine <1324789704@qq.com>
2025-08-27 17:49:24 +0800

    Select算子加固：常量输入

--- tensorflow/python/grappler/embedding_fused_test/fused_embedding_sparse_select_test.py
+++ tensorflow/python/grappler/embedding_fused_test/fused_embedding_sparse_select_test.py
@@ -10,35 +10,36 @@ from utils.utils import benchmark_op
 np.random.seed(140)
 
 
-def ori_sparse_select_graph(input_a, input_b, input_c):
+def ori_sparse_select_graph(input_a, input_b, input_c, greater, equal1, equal2, equal3):
     a = tf.reshape(input_a, [-1, 1])
     b = tf.reshape(input_b, [-1, 1])
     c = tf.reshape(input_c, [-1, 1])
+    output_x = a
 
-    greater_a = tf.greater(a, 0)
+    greater_a = tf.greater(a, greater)
     shape_reshape_a1 = tf.shape(a)
     shape_reshape_a2 = tf.shape(a)
     fill_a1 = tf.fill(shape_reshape_a1, tf.constant(1, dtype=tf.float32))
     realdiv = tf.realdiv(fill_a1, tf.constant(1, dtype=tf.float32))
-    output_x = tf.fill(shape_reshape_a2, tf.constant(0, dtype=tf.float32))  # 全部填充为0，第一个输出
     cast_a = tf.cast(greater_a, tf.float32)
     shape_a = tf.shape(cast_a)
     fill_a = tf.fill(shape_a, tf.constant(1, dtype=tf.float32))
-    equal_4563 = tf.equal(b, 4563)
-    equal_10831 = tf.equal(b, 10831)
-    equal_3 = tf.equal(c, 3)
+    equal_4563 = tf.equal(b, equal1)
+    equal_10831 = tf.equal(b, equal2)
+    equal_3 = tf.equal(c, equal3)
     select_1 = tf.where(equal_4563, fill_a, cast_a)
     select_2 = tf.where(equal_10831, fill_a, select_1)
-    output_y = tf.subtract(tf.constant(1, dtype=tf.float32), select_2)  # 1 - select_2 ？
+    output_y = select_2  # select_2
     mul = tf.multiply(tf.constant(1, dtype=tf.float32), select_2)  # Select.2415 --> Mul.2419
-    select_3 = tf.where(equal_3, realdiv, fill_a1)  # realdiv和fill_a1 一样的？
+    select_3 = tf.where(equal_3, realdiv, fill_a1)
     output_z = tf.concat([mul, select_3], axis=-1)
     return output_x, output_y, output_z
 
 
-def opt_sparse_select_graph(input_a, input_b, input_c):
+def opt_sparse_select_graph(input_a, input_b, input_c, greater, equal1, equal2, equal3):
     output_x, output_y, output_z = gen_embedding_fused_ops.KPFusedSparseSelect(
-        input_a=input_a, input_b=input_b, input_c=input_c
+        input_a=input_a, input_b=input_b, input_c=input_c, greater=greater,
+        equal1=equal1, equal2=equal2, equal3=equal3
     )
     return output_x, output_y, output_z
 
@@ -64,18 +65,29 @@ class TestKPFusedSparseSelect(unittest.TestCase):
             input0 = tf.compat.v1.placeholder(tf.int32, name="input_a")
             input1 = tf.compat.v1.placeholder(tf.int32, name="input_b")
             input2 = tf.compat.v1.placeholder(tf.int32, name="input_c")
+            greater = tf.compat.v1.placeholder(tf.int32, shape=(), name="greater")
+            equal1 = tf.compat.v1.placeholder(tf.int32, shape=(), name="equal1")
+            equal2 = tf.compat.v1.placeholder(tf.int32, shape=(), name="equal2")
+            equal3 = tf.compat.v1.placeholder(tf.int32, shape=(), name="equal3")
             """Initialize test data"""
             feed = {
                 input0: np.random.randint(0, 100, size=a_shape).astype(np.int32),
                 input1: np.random.randint(0, 100, size=b_shape).astype(np.int32),
                 input2: np.random.randint(0, 100, size=c_shape).astype(np.int32),
+                greater: np.array(0, dtype=np.int32),
+                equal1: np.array(4563, dtype=np.int32),
+                equal2: np.array(10831, dtype=np.int32),
+                equal3: np.array(3, dtype=np.int32),
             }
 
             with tf.name_scope("ori"):
-                out_ori1, out_ori2, out_ori3 = ori_sparse_select_graph(input0, input1, input2)
+                out_ori1, out_ori2, out_ori3 = ori_sparse_select_graph(
+                    input0, input1, input2, greater, equal1, equal2, equal3
+                )
             with tf.name_scope("opt"):
-                out_opt1, out_opt2, out_opt3 = opt_sparse_select_graph(input0, input1, input2)
-            
+                out_opt1, out_opt2, out_opt3 = opt_sparse_select_graph(
+                    input0, input1, input2, greater, equal1, equal2, equal3
+                )
             with tf.compat.v1.Session(config=self.config) as sess:
                 out_ori_val1, out_ori_val2, out_ori_val3 = sess.run(
                     [out_ori1, out_ori2, out_ori3], 
@@ -84,7 +96,7 @@ class TestKPFusedSparseSelect(unittest.TestCase):
                     run_metadata=self.run_metadata_ori
                 )
                 out_opt_val1, out_opt_val2, out_opt_val3 = sess.run(
-                    [out_opt1,out_opt2, out_opt3], 
+                    [out_opt1, out_opt2, out_opt3], 
                     feed_dict=feed, 
                     options=self.run_options, 
                     run_metadata=self.run_metadata_opt
@@ -122,7 +134,7 @@ class TestKPFusedSparseSelect(unittest.TestCase):
                     self.run_metadata_opt,
                     op_name="KPFusedSparseSelect",
                     start_op="ori/Reshape",
-                    end_op="ori/Sub",
+                    end_op="ori/concat",
                     num_runs=num_runs,
                     tag="-----TF_origin-----"
                 )
